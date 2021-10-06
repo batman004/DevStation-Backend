f6357f28137c0eceec983328096ef1fb2dcd616a
batman004 <pathania.yuvraj@gmail.com>
2021-10-06 13:07:50 +0530

    fix: add CORS middleware

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.posts.routers import router as posts_router
 from config import settings
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_db_client():
