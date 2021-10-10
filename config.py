a1ef37a675edcf5ecd00bac18d0e85147b0f106e
batman004 <pathania.yuvraj@gmail.com>
2021-10-10 13:00:36 +0530

    update: config.py for prod

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import dotenv_values
 import os
 
 config = dotenv_values(".env") 
+prod = False
 
 class CommonSettings(BaseSettings):
     APP_NAME: str = "DevStation"
@@ -13,14 +14,14 @@ class ServerSettings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
 
+if prod ==True:
+    class DatabaseSettings(BaseSettings):
+        DB_URL: str = os.environ['DB_URL']
+        DB_NAME: str = os.environ['DB_NAME'] 
+
 class DatabaseSettings(BaseSettings):
-    DB_URL: str = os.environ['DB_URL']
-    DB_NAME: str = os.environ['DB_NAME'] 
-
-
-# class DatabaseSettings(BaseSettings):
-#     DB_URL: str = config['DB_URL']
-#     DB_NAME: str = config['DB_NAME'] 
+    DB_URL: str = config['DB_URL']
+    DB_NAME: str = config['DB_NAME'] 
 
 class Settings(CommonSettings, ServerSettings, DatabaseSettings):
     pass
