b41e62e8580e819b608edbb4113c111e90cb9361
batman004 <pathania.yuvraj@gmail.com>
2021-10-10 16:58:02 +0530

    fix: add env vars for prod

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import dotenv_values
 import os
 
 config = dotenv_values(".env") 
+prod = True
 
 class CommonSettings(BaseSettings):
     APP_NAME: str = "DevStation"
@@ -13,14 +14,15 @@ class ServerSettings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
 
-class DatabaseSettings(BaseSettings):
-    DB_URL: str = os.environ['DB_URL']
-    DB_NAME: str = os.environ['DB_NAME'] 
+if prod ==True:
+    class DatabaseSettings(BaseSettings):
+        DB_URL: str = os.environ['DB_URL']
+        DB_NAME: str = os.environ['DB_NAME'] 
 
-
-# class DatabaseSettings(BaseSettings):
-#     DB_URL: str = config['DB_URL']
-#     DB_NAME: str = config['DB_NAME'] 
+else :
+    class DatabaseSettings(BaseSettings):
+        DB_URL: str = config['DB_URL']
+        DB_NAME: str = config['DB_NAME'] 
 
 class Settings(CommonSettings, ServerSettings, DatabaseSettings):
     pass
